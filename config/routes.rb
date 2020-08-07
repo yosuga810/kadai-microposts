3f8f660a62ab399b6e3052f1471b667b6dcb074c
yasu.suzuki <yosuga810@gmail.com>
2020-08-07 12:48:29 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, only: [:index, :show, :new, :create]
 end
+
